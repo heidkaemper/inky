a902bd9d381652cb5eaa70f3629f0ff97ac50535
Lars Heidkämper <heidkaemper@new-data-services.de>
2024-11-24 23:39:29 +0100

    add arguments to main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+import argparse
 from ai import show_ai
+from comic import show_comic
+from status import show_status
 
-show_ai()
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--type", "-t", choices=['ai', 'comic', 'status'], default='ai')
+
+args = parser.parse_args()
+
+match args.type:
+    case 'ai':
+        show_ai()
+    case 'comic':
+        show_comic()
+    case 'status':
+        show_status()
